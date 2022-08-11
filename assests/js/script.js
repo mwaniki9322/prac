a725e849d60402283567e57e39866eef35003edb
Dennis Mwaniki <damos5776@gmail.com>
2022-08-11 20:08:02 +0300

    wewe

---
+++ assests/js/script.js
@@ -0,0 +1,17 @@
+function welcome(){
+    alert('welcome to my portfolio')
+}
+welcome()
+
+
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
